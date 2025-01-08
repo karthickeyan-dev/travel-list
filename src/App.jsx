7158b84e10521022edd5fdf7308aaa183027e98b
Karthic <jkarthickeyan24@gmail.com>
2025-01-08 16:42:11 +0530

    Add packing list functionality with item input form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-// import './index.css';
+import { useState } from 'react';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, isPacked: false },
+  { id: 2, description: 'Socks', quantity: 12, isPacked: false },
+  { id: 3, description: 'Charger', quantity: 1, isPacked: true },
+  { id: 4, description: 'Phone', quantity: 1, isPacked: false },
+];
 
 export default function App() {
   return (
@@ -14,20 +21,77 @@ export default function App() {
 function Logo() {
   return <h1>✈️ Far Away 🌴</h1>;
 }
+
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+
+    const newItem = {
+      quantity,
+      description,
+      isPacked: false,
+      id: Date.now(),
+    };
+
+    setQuantity(1);
+    setDescription('');
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip? </h3>
+      <select
+        value={quantity}
+        onChange={e => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
     </div>
   );
 }
-function PackingList() {
-  return <div className="list">LIST</div>;
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.isPacked ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
+
 function Stats() {
   return (
     <footer className="stats">
-      <em>You have X items of your list, and you already packed X (X%)</em>
+      <em>💼 You have X items of your list, and you already packed X (X%)</em>
     </footer>
   );
 }
