91bc839ae6cde96dd670bed516eb31ad14bfebbf
Karthic <jkarthickeyan24@gmail.com>
2025-01-09 14:51:55 +0530

    Enhance Stats component to display item count and packing percentage

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,13 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
 
 function Logo() {
-  return <h1>✈️ Far Away 🌴</h1>;
+  return <h1>🏖️ Far Away 🌴</h1>;
 }
 
 function Form({ onAddItem }) {
@@ -114,10 +114,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter(item => item.isPacked).length;
+  const percentage = Math.ceil((numPacked / numItems) * 100) || 0;
   return (
     <footer className="stats">
-      <em>💼 You have X items of your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? 'You got everything packed, Ready to go ✈️'
+          : `💼 You have ${numItems} items of your list, and you already pack ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
